2aafe1c8935272358560f4d503e1d836dcfd5cc8
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2023-06-23 18:00:36 +0330

    implement message service

---
+++ src/main/java/com/chatkon/backend/service/message/MessageService.java
@@ -0,0 +1,18 @@
+package com.chatkon.backend.service.message;
+
+import com.chatkon.backend.model.entity.message.BinaryMessage;
+import com.chatkon.backend.model.entity.message.Message;
+import com.chatkon.backend.model.entity.message.TextMessage;
+
+public interface MessageService {
+
+    TextMessage createText(Long chatId, Long senderId, TextMessage textMessage);
+
+    BinaryMessage saveFile(BinaryMessage binaryMessage);
+
+    Message getLastMessage(Long chatId);
+
+    Long deleteMessage(Long deleterId, Long messageId);
+
+    Message findMessage(Long messageId);
+}
